3d54aee45b564f536e3e18c4af59284621150dde
Bulat <bulat0talipov@gmail.com>
2022-10-17 09:48:22 +0300

    added changes to the first block and added a content block

--- src/js/main.js
+++ src/js/main.js
@@ -1,133 +1,209 @@
-import { Swiper, Navigation, Pagination, Autoplay, EffectFade } from 'swiper';
+import {
+  Swiper,
+  Navigation,
+  Pagination,
+  Autoplay,
+  EffectFade
+} from 'swiper';
 import HystModal from './HystModal';
 import Forms from './Forms';
-import { Map } from './Map';
+import {
+  Map
+} from './Map';
 import debug from './utils/debug';
 import Accordions from './Accordions';
 
-document.addEventListener('DOMContentLoaded', function() {
-    window.itmo = {}; // Тут будут лежать всякие функции с фронта
+document.addEventListener('DOMContentLoaded', function () {
+  window.itmo = {}; // Тут будут лежать всякие функции с фронта
 
-    // document.header = document.querySelector('.header');
+  // document.header = document.querySelector('.header');
 
-    modals();
-    forms();
-    maps();
-    debug(); // Нажми 5 раз "d" на клавиатуре
-    accordions();
-    searchFields();
-    miniSliders();
+  modals();
+  forms();
+  maps();
+  debug(); // Нажми 5 раз "d" на клавиатуре
+  accordions();
+  searchFields();
+  miniSliders();
+  contentSliders();
 });
 
 function modals() {
-    new HystModal({
-        beforeOpen: instance => {
-            // const target = instance.starter;
-            //
-            // if (target.classList.contains('js-reset-by-modal')) {
-            //     const form = target.closest('form');
-            //     form && form.reset();
-            // }
-        }
-    });
+  new HystModal({
+    beforeOpen: instance => {
+      // const target = instance.starter;
+      //
+      // if (target.classList.contains('js-reset-by-modal')) {
+      //     const form = target.closest('form');
+      //     form && form.reset();
+      // }
+    }
+  });
 }
 
 function forms() {
-    new Forms({
-        onSuccess: (form, response) => {
-            modals.open('#success');
-        },
-        onError: (form, response) => {
-            modals.open('#error');
-        }
-    });
+  new Forms({
+    onSuccess: (form, response) => {
+      modals.open('#success');
+    },
+    onError: (form, response) => {
+      modals.open('#error');
+    }
+  });
 }
 
 function maps() {
-    ymaps.ready(() => {
-        [...document.querySelectorAll('.js-map')].forEach(map => {
-            const instance = new Map(map, {
-                icon: {
-                    url: 'img/placemark.svg'
-                }
-            });
-            instance.addPlace([55.76, 37.64]);
-        });
+  ymaps.ready(() => {
+    [...document.querySelectorAll('.js-map')].forEach(map => {
+      const instance = new Map(map, {
+        icon: {
+          url: 'img/placemark.svg'
+        }
+      });
+      instance.addPlace([55.76, 37.64]);
     });
+  });
 }
 
 function accordions() {
-    new Accordions({
-        selectors: {
-            container: '.js-footer-accordions',
-            wrapper: '.js-accordion',
-            button: '.js-accordion-btn',
-            content: '.js-accordion-content'
-        }
-    });
+  new Accordions({
+    selectors: {
+      container: '.js-footer-accordions',
+      wrapper: '.js-accordion',
+      button: '.js-accordion-btn',
+      content: '.js-accordion-content'
+    }
+  });
 }
 
 function miniSliders() {
-    const miniSliders = document.querySelectorAll('.js-mini-slider');
-    miniSliders.forEach(slider => {
-        new Swiper(slider.querySelector('.swiper'), {
-            loop: true,
-            speed: 500,
-            slidesPerView: 1,
-            spaceBetween: 0,
-            modules: [Navigation, Pagination, Autoplay, EffectFade],
-            navigation: {
-                nextEl: '.js-mini-slider .next',
-                prevEl: '.js-mini-slider .prev'
-            },
-            pagination: {
-                el: '.js-mini-slider .mini-slider__pagination',
-                type: 'fraction',
-                formatFractionCurrent: function(n) {
-                    return (n < 10 ? '0' : '') + n;
-                },
-                formatFractionTotal: function(n) {
-                    return (n < 10 ? '0' : '') + n;
-                }
-            },
-            autoplay: {
-                delay: 5000,
-                disableOnInteraction: true,
-                pauseOnMouseEnter: true
-            },
-            effect: 'fade',
-            fadeEffect: {
-                crossFade: true
-            },
-            on: {
-                init: function (swiper) {
-                    swiper.el.classList.remove("loading")
-                },
-            },
-        });
+  const miniSliders = document.querySelectorAll('.js-mini-slider');
+  miniSliders.forEach(slider => {
+    new Swiper(slider.querySelector('.swiper'), {
+      loop: true,
+      speed: 500,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      modules: [Navigation, Pagination, Autoplay, EffectFade],
+      navigation: {
+        nextEl: '.js-mini-slider .next',
+        prevEl: '.js-mini-slider .prev'
+      },
+      pagination: {
+        el: '.js-mini-slider .mini-slider__pagination',
+        type: 'fraction',
+        formatFractionCurrent: function (n) {
+          return (n < 10 ? '0' : '') + n;
+        },
+        formatFractionTotal: function (n) {
+          return (n < 10 ? '0' : '') + n;
+        }
+      },
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: true,
+        pauseOnMouseEnter: true
+      },
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      on: {
+        init: function (swiper) {
+          swiper.el.classList.remove("loading")
+        },
+      },
     });
+  });
 }
 
-function searchFields() {
-    const containers = document.querySelectorAll('.js-search');
-
-    containers.forEach(container => {
-        const openButton = container.querySelector('.search-field__button--open');
-        const closeButton = container.querySelector('.search-field__button--close');
-        const input = container.querySelector('.search-field__element');
-
-        openButton.addEventListener('click', () => {
-            document.body.classList.add('js-search-filed-opened');
-            container.classList.add('active');
-            openButton.setAttribute('disabled', 'true');
-            closeButton.removeAttribute('disabled');
-            input.focus();
-        });
-        closeButton.addEventListener('click', () => {
-            container.classList.remove('active');
-            document.body.classList.remove('js-search-filed-opened');
-            openButton.removeAttribute('disabled');
-            closeButton.setAttribute('disabled', 'true');
-        });
+function contentSliders() {
+  const contentSliders = document.querySelectorAll('.content-slider__slider');
+  contentSliders.forEach(slider => {
+    const contentSwiper = new Swiper(slider.querySelector('.content-swiper'), {
+      loop: true,
+      speed: 500,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      modules: [Navigation, Pagination, Autoplay, EffectFade],
+      navigation: {
+        nextEl: '.content-slider__slider .next',
+        prevEl: '.content-slider__slider .prev'
+      },
+      pagination: {
+        el: '.content-slider__slider .content-slider__pagination',
+        type: 'fraction',
+        renderFraction: function (currentClass, totalClass) {
+            return '<span class="' + currentClass + '"></span>'
+        },
+        totalClass: false,
+        formatFractionCurrent: function (n) {
+          return (n < 10 ? '0' : '') + n;
+        },
+      },
+      autoplay: {
+          delay: 5000,
+          disableOnInteraction: true,
+          pauseOnMouseEnter: true
+      },
+      on: {
+        init: function (swiper) {
+          swiper.el.classList.remove("loading")
+        },
+      },
     });
+
+    const sliderText  = new Swiper(slider.querySelector('.content-slider__text-block'), {
+        loop: true,
+        speed: 500,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: true,
+            pauseOnMouseEnter: true
+        },
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+        on: {
+          init: function (swiper) {
+            swiper.el.classList.remove("loading")
+          },
+        },
+      });
+
+      contentSwiper.controller.control = sliderText;
+      sliderText.controller.control = contentSwiper;
+
+  });
+}
+
+
+
+
+
+function searchFields() {
+  const containers = document.querySelectorAll('.js-search');
+
+  containers.forEach(container => {
+    const openButton = container.querySelector('.search-field__button--open');
+    const closeButton = container.querySelector('.search-field__button--close');
+    const input = container.querySelector('.search-field__element');
+
+    openButton.addEventListener('click', () => {
+      document.body.classList.add('js-search-filed-opened');
+      container.classList.add('active');
+      openButton.setAttribute('disabled', 'true');
+      closeButton.removeAttribute('disabled');
+      input.focus();
+    });
+    closeButton.addEventListener('click', () => {
+      container.classList.remove('active');
+      document.body.classList.remove('js-search-filed-opened');
+      openButton.removeAttribute('disabled');
+      closeButton.setAttribute('disabled', 'true');
+    });
+  });
 }
