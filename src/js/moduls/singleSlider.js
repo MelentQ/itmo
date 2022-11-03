35e2e146b928b16437dd95fa8d149d13e8265adb
Bulat <bulat0talipov@gmail.com>
2022-11-03 09:36:43 +0300

    added new sections

--- src/js/moduls/singleSlider.js
+++ src/js/moduls/singleSlider.js
@@ -1,25 +1,58 @@
 import {
     Swiper,
-    Navigation
+    Navigation,
+    EffectFade,
+    Controller
   } from 'swiper';
-  
-  export default function singleSlider() {
-    const singleSlider = document.querySelectorAll('.quote-single__slider');
-    singleSlider.forEach(slider => {
-      new Swiper(slider.querySelector('.quote-single__swiper'), {
-        speed: 500,
-        slidesPerView: 1,
-        spaceBetween: 50,
-        modules: [Navigation],
-        navigation: {
-          nextEl: '.quote-single__slider .next',
-          prevEl: '.quote-single__slider .prev'
-        },
-        on: {
-          init: function (swiper) {
-            swiper.el.classList.remove("loading")
-          },
+
+export default function singleSlider() {
+  const singleSlider = document.querySelectorAll('.quote-single__slider');
+  singleSlider.forEach(slider => {
+    const textSwiper = new Swiper(slider.querySelector('.quote-single__swiper-text'), {
+      speed: 500,
+      slidesPerView: 1,
+      spaceBetween: 10,
+      modules: [Navigation, Controller, EffectFade],
+      effect: 'fade',
+      autoHeight: true,
+      fadeEffect: {
+        crossFade: true
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 1,
         }
-      });
+      },
+      navigation: {
+        nextEl: '.quote-single__slider .next',
+        prevEl: '.quote-single__slider .prev'
+      },
+      on: {
+        init: function (swiper) {
+          swiper.el.classList.remove("loading")
+        },
+      }
     });
-  }
+
+    const sliderVideo = new Swiper(slider.querySelector('.quote-single__swiper-video'), {
+      speed: 500,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      modules: [ EffectFade, Controller],
+      effect: 'fade',
+      autoHeight: true,
+      fadeEffect: {
+        crossFade: true
+      },
+      on: {
+        init: function (swiper) {
+          swiper.el.classList.remove("loading")
+        },
+      }
+    });
+
+    textSwiper.controller.control = sliderVideo;
+    sliderVideo.controller.control = textSwiper;
+
+  });
+}
